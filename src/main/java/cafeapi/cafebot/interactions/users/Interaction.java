63811345e24bdab146ff7e74438acd188cfd57c0
William Orlopp <beanbeanjuice@outlook.com>
2021-08-24 10:58:26 -0700

    Completed the Interactions API Wrapper

---
+++ src/main/java/cafeapi/cafebot/interactions/users/Interaction.java
@@ -0,0 +1,335 @@
+package cafeapi.cafebot.interactions.users;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A class used to hold {@link Interaction} information.
+ *
+ * @author beanbeanjuice
+ */
+public class Interaction {
+
+    private final Integer hugAmount;
+    private final Integer punchAmount;
+    private final Integer kissAmount;
+    private final Integer biteAmount;
+    private final Integer blushAmount;
+    private final Integer cuddleAmount;
+    private final Integer nomAmount;
+    private final Integer pokeAmount;
+    private final Integer slapAmount;
+    private final Integer stabAmount;
+    private final Integer hmphAmount;
+    private final Integer poutAmount;
+    private final Integer throwAmount;
+    private final Integer smileAmount;
+    private final Integer stareAmount;
+    private final Integer tickleAmount;
+    private final Integer rageAmount;
+    private final Integer yellAmount;
+    private final Integer headpatAmount;
+    private final Integer cryAmount;
+    private final Integer danceAmount;
+    private final Integer dabAmount;
+    private final Integer bonkAmount;
+    private final Integer sleepAmount;
+    private final Integer dieAmount;
+    private final Integer welcomeAmount;
+    private final Integer lickAmount;
+    private final Integer shushAmount;
+
+    /**
+     * Creates a new {@link Interaction object.}
+     * @param hugAmount The {@link Integer hugAmount}.
+     * @param punchAmount The {@link Integer punchAmount}.
+     * @param kissAmount The {@link Integer kissAmount}.
+     * @param biteAmount The {@link Integer biteAmount}.
+     * @param blushAmount The {@link Integer blushAmount}.
+     * @param cuddleAmount The {@link Integer cuddleAmount}.
+     * @param nomAmount The {@link Integer nomAmount}.
+     * @param pokeAmount The {@link Integer pokeAmount}.
+     * @param slapAmount The {@link Integer slapAmount}.
+     * @param stabAmount The {@link Integer stabAmount}.
+     * @param hmphAmount The {@link Integer hmphAmount}.
+     * @param poutAmount The {@link Integer poutAmount}.
+     * @param throwAmount The {@link Integer throwAmount}.
+     * @param smileAmount The {@link Integer smileAmount}.
+     * @param stareAmount The {@link Integer stareAmount}.
+     * @param tickleAmount The {@link Integer tickleAmount}.
+     * @param rageAmount The {@link Integer rageAmount}.
+     * @param yellAmount The {@link Integer yellAmount}.
+     * @param headpatAmount The {@link Integer headpatAmount}.
+     * @param cryAmount The {@link Integer cryAmount}.
+     * @param danceAmount The {@link Integer danceAmount}.
+     * @param dabAmount The {@link Integer dabAmount}.
+     * @param bonkAmount The {@link Integer bonkAmount}.
+     * @param sleepAmount The {@link Integer sleepAmount}.
+     * @param dieAmount The {@link Integer dieAmount}.
+     * @param welcomeAmount The {@link Integer welcomeAmount}.
+     * @param lickAmount The {@link Integer lickAmount}.
+     * @param shushAmount The {@link Integer shushAmount}.
+     */
+    public Interaction(Integer hugAmount, Integer punchAmount, Integer kissAmount,
+                       Integer biteAmount, Integer blushAmount, Integer cuddleAmount,
+                       Integer nomAmount, Integer pokeAmount, Integer slapAmount,
+                       Integer stabAmount, Integer hmphAmount, Integer poutAmount,
+                       Integer throwAmount, Integer smileAmount, Integer stareAmount,
+                       Integer tickleAmount, Integer rageAmount, Integer yellAmount,
+                       Integer headpatAmount, Integer cryAmount, Integer danceAmount,
+                       Integer dabAmount, Integer bonkAmount, Integer sleepAmount,
+                       Integer dieAmount, Integer welcomeAmount, Integer lickAmount,
+                       Integer shushAmount) {
+        this.hugAmount = hugAmount;
+        this.punchAmount = punchAmount;
+        this.kissAmount = kissAmount;
+        this.biteAmount = biteAmount;
+        this.blushAmount = blushAmount;
+        this.cuddleAmount = cuddleAmount;
+        this.nomAmount = nomAmount;
+        this.pokeAmount = pokeAmount;
+        this.slapAmount = slapAmount;
+        this.stabAmount = stabAmount;
+        this.hmphAmount = hmphAmount;
+        this.poutAmount = poutAmount;
+        this.throwAmount = throwAmount;
+        this.smileAmount = smileAmount;
+        this.stareAmount = stareAmount;
+        this.tickleAmount = tickleAmount;
+        this.rageAmount = rageAmount;
+        this.yellAmount = yellAmount;
+        this.headpatAmount = headpatAmount;
+        this.cryAmount = cryAmount;
+        this.danceAmount = danceAmount;
+        this.dabAmount = dabAmount;
+        this.bonkAmount = bonkAmount;
+        this.sleepAmount = sleepAmount;
+        this.dieAmount = dieAmount;
+        this.welcomeAmount = welcomeAmount;
+        this.lickAmount = lickAmount;
+        this.shushAmount = shushAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer hugAmount}.
+     */
+    @NotNull
+    public Integer getHugAmount() {
+        return hugAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer punchAmount}.
+     */
+    @NotNull
+    public Integer getPunchAmount() {
+        return punchAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer kissAmount}.
+     */
+    @NotNull
+    public Integer getKissAmount() {
+        return kissAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer biteAmount}.
+     */
+    @NotNull
+    public Integer getBiteAmount() {
+        return biteAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer blushAmount}.
+     */
+    @NotNull
+    public Integer getBlushAmount() {
+        return blushAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer cuddleAmount}.
+     */
+    @NotNull
+    public Integer getCuddleAmount() {
+        return cuddleAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer nomAmount}.
+     */
+    @NotNull
+    public Integer getNomAmount() {
+        return nomAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer pokeAmount}.
+     */
+    @NotNull
+    public Integer getPokeAmount() {
+        return pokeAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer slapAmount}.
+     */
+    @NotNull
+    public Integer getSlapAmount() {
+        return slapAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer stabAmount}.
+     */
+    @NotNull
+    public Integer getStabAmount() {
+        return stabAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer hmphAmount}.
+     */
+    @NotNull
+    public Integer getHmphAmount() {
+        return hmphAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer poutAmount}.
+     */
+    @NotNull
+    public Integer getPoutAmount() {
+        return poutAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer throwAmount}.
+     */
+    @NotNull
+    public Integer getThrowAmount() {
+        return throwAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer smileAmount}.
+     */
+    @NotNull
+    public Integer getSmileAmount() {
+        return smileAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer stareAmount}.
+     */
+    @NotNull
+    public Integer getStareAmount() {
+        return stareAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer tickleAmount}.
+     */
+    @NotNull
+    public Integer getTickleAmount() {
+        return tickleAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer rageAmount}.
+     */
+    @NotNull
+    public Integer getRageAmount() {
+        return rageAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer yellAmount}.
+     */
+    @NotNull
+    public Integer getYellAmount() {
+        return yellAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer headpatAmount}.
+     */
+    @NotNull
+    public Integer getHeadpatAmount() {
+        return headpatAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer cryAmount}.
+     */
+    @NotNull
+    public Integer getCryAmount() {
+        return cryAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer danceAmount}.
+     */
+    @NotNull
+    public Integer getDanceAmount() {
+        return danceAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer dabAmount}.
+     */
+    @NotNull
+    public Integer getDabAmount() {
+        return dabAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer bonkAmount}.
+     */
+    @NotNull
+    public Integer getBonkAmount() {
+        return bonkAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer sleepAmount}.
+     */
+    @NotNull
+    public Integer getSleepAmount() {
+        return sleepAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer dieAmount}.
+     */
+    @NotNull
+    public Integer getDieAmount() {
+        return dieAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer welcomeAmount}.
+     */
+    @NotNull
+    public Integer getWelcomeAmount() {
+        return welcomeAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer lickAmount}.
+     */
+    @NotNull
+    public Integer getLickAmount() {
+        return lickAmount;
+    }
+
+    /**
+     * @return The {@link Interaction} {@link Integer shushAmount}.
+     */
+    @NotNull
+    public Integer getShushAmount() {
+        return shushAmount;
+    }
+}
