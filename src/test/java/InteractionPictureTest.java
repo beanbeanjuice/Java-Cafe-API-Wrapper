fe9178c6b9a4d7cd2136c0251ecb4d1de49605af
William Orlopp <beanbeanjuice@outlook.com>
2021-08-29 20:11:04 -0700

    Updated the Unit Test for InteractionPictures

--- src/test/java/InteractionPictureTest.java
+++ src/test/java/InteractionPictureTest.java
@@ -11,7 +11,12 @@ public class InteractionPictureTest {
     public void testInteractionPicturesAPI() {
         CafeAPI cafeAPI = new CafeAPI("beanbeanjuice", "password123");
 
-        Assertions.assertNotNull(cafeAPI.interactionPictures().getRandomInteractionPicture(InteractionType.SHUSH));
+        // Goes through every interaction type.
+        Assertions.assertDoesNotThrow(() -> {
+            for (InteractionType type : InteractionType.values()) {
+                Assertions.assertNotNull(cafeAPI.interactionPictures().getRandomInteractionPicture(type));
+            }
+        });
     }
 
 }
