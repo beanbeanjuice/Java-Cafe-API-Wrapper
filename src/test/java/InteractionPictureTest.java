66b0ed14c48eb6d124f3514183981e728eac95d4
William Orlopp <beanbeanjuice@outlook.com>
2021-08-29 20:04:50 -0700

    Created the Interaction Pictures API Wrapper

---
+++ src/test/java/InteractionPictureTest.java
@@ -0,0 +1,18 @@
+import cafeapi.CafeAPI;
+import cafeapi.cafebot.interactions.InteractionType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InteractionPictureTest {
+
+    @Test
+    @DisplayName("Interaction Pictures API Test")
+    public void testInteractionPicturesAPI() {
+        CafeAPI cafeAPI = new CafeAPI("beanbeanjuice", "password123");
+
+        Assertions.assertNotNull(cafeAPI.interactionPictures().getInteractionPicture(InteractionType.SHUSH));
+        System.out.println(cafeAPI.interactionPictures().getInteractionPicture(InteractionType.SLAP));
+    }
+
+}
