afcdefeb850ebcda8471423b55fc129bd8785356
beanbeanjuice <beanbeanjuice@outlook.com>
2021-09-13 23:55:59 -0700

    Updated Counting Wrapper

--- src/main/java/io/github/beanbeanjuice/cafeapi/cafebot/counting/CountingInformation.java
+++ src/main/java/io/github/beanbeanjuice/cafeapi/cafebot/counting/CountingInformation.java
@@ -12,17 +12,21 @@ public class CountingInformation {
     private final Integer highestNumber;
     private final Integer lastNumber;
     private final String lastUserID;
+    private final String failureRoleID;
 
     /**
      * Creates a new {@link CountingInformation} object.
      * @param highestNumber The {@link Integer highestNumber}.
      * @param lastNumber The {@link Integer lastNumber}.
      * @param lastUserID The {@link String lastUserID}.
+     * @param failureRoleID The {@link String failureRoleID}.
      */
-    public CountingInformation(@NotNull Integer highestNumber, @NotNull Integer lastNumber, @NotNull String lastUserID) {
+    public CountingInformation(@NotNull Integer highestNumber, @NotNull Integer lastNumber, @NotNull String lastUserID,
+                               @NotNull String failureRoleID) {
         this.highestNumber = highestNumber;
         this.lastNumber = lastNumber;
         this.lastUserID = lastUserID;
+        this.failureRoleID = failureRoleID;
     }
 
     /**
@@ -48,4 +52,13 @@ public class CountingInformation {
     public String getLastUserID() {
         return lastUserID;
     }
+
+    /**
+     * @return The {@link String failureRoleID}.
+     */
+    @NotNull
+    public String getFailureRoleID() {
+        return failureRoleID;
+    }
+
 }
