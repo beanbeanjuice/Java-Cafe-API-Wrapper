f6f42d7e73f2838dba17d87aa0502ffc160b68a7
beanbeanjuice <beanbeanjuice@outlook.com>
2022-07-09 19:15:11 -0400

    Added Javadocs to New Exceptions

--- src/main/java/io/github/beanbeanjuice/cafeapi/exception/program/BirthdayOverfillException.java
+++ src/main/java/io/github/beanbeanjuice/cafeapi/exception/program/BirthdayOverfillException.java
@@ -2,12 +2,24 @@ package io.github.beanbeanjuice.cafeapi.exception.program;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A {@link RuntimeException} that deals with incorrect days in a {@link io.github.beanbeanjuice.cafeapi.cafebot.birthdays.BirthdayMonth BirthdayMonth}.
+ *
+ * @author beanbeanjuice
+ */
 public class BirthdayOverfillException extends RuntimeException {
 
+    /**
+     * Creates a new {@link BirthdayOverfillException}.
+     * @param message The message to send to the {@link RuntimeException}.
+     */
     public BirthdayOverfillException(@NotNull String message) {
         super(message);
     }
 
+    /**
+     * Creates a new {@link BirthdayOverfillException}.
+     */
     public BirthdayOverfillException() {
         super("More days than there are days in the month.");
     }
--- src/main/java/io/github/beanbeanjuice/cafeapi/exception/program/InvalidTimeZoneException.java
+++ src/main/java/io/github/beanbeanjuice/cafeapi/exception/program/InvalidTimeZoneException.java
@@ -2,12 +2,24 @@ package io.github.beanbeanjuice.cafeapi.exception.program;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * A {@link RuntimeException} for dealing with invalid {@link java.util.TimeZone TimeZone}.
+ *
+ * @author beanbeanjuice
+ */
 public class InvalidTimeZoneException extends RuntimeException {
 
+    /**
+     * Creates a new {@link InvalidTimeZoneException}.
+     * @param message The message to send to the {@link RuntimeException}.
+     */
     public InvalidTimeZoneException(@NotNull String message) {
         super(message);
     }
 
+    /**
+     * Creates a new {@link InvalidTimeZoneException}.
+     */
     public InvalidTimeZoneException() {
         super("The timezone is invalid!");
     }
