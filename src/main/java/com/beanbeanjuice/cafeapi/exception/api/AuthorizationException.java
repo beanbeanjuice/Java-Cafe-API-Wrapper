1a4e06ab656e3e9db21ba397227b338ef39d0631
beanbeanjuice <beanbeanjuice@outlook.com>
2022-07-15 10:42:15 -0400

    Changed OSSRH Repository

---
+++ src/main/java/com/beanbeanjuice/cafeapi/exception/api/AuthorizationException.java
@@ -0,0 +1,21 @@
+package com.beanbeanjuice.cafeapi.exception.api;
+
+import com.beanbeanjuice.cafeapi.requests.Request;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A {@link CafeException} used when a {@link Request Request} responds with a status code of 401.
+ *
+ * @author beanbeanjuice
+ */
+public class AuthorizationException extends CafeException {
+
+    /**
+     * Creates a new {@link AuthorizationException}.
+     * @param request The {@link Request} that threw the {@link CafeException}.
+     */
+    public AuthorizationException(@NotNull Request request) {
+        super(request);
+    }
+
+}
